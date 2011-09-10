375df8cafdf63bbe44fe8b624f308c8b7defc5ff
jumanbar <jumanbar@gmail.com>
2011-09-10 16:40:22 -0300

    Mejoras a cohercionadores & identificadores:
    - 80 caracteres de ancho.
    - Observación sobre coherciones imposibles.

--- lecciones/choercionadores-e-identificadores.R
+++ lecciones/choercionadores-e-identificadores.R
@@ -1,8 +1,14 @@
-#### cohercionadores
-## Muchas veces nos interesa convertir un vector de una clase a otra. Existe en R una serie de funciones que sirven para hacer estas transformaciones.
-## Estas funciones, si pueden, "cohercionan" el objeto de entrada, generando un objeto de salida de la clase deseada.
-## El nombre de estas funciones sigue la pauta: "as.clase", en donde la palabra "clase" debe ser sustituida por una clase verdadera...
-## Por ejemplo:
+#### Cohercionadores
+
+# Muchas veces nos interesa convertir un vector de una clase a otra. Existe
+# en R una serie de funciones que sirven para hacer estas transformaciones.
+# Estas funciones, si pueden, "cohercionan" el objeto de entrada, generando
+# un objeto de salida de la clase deseada.
+
+# El nombre de estas funciones sigue la pauta: "as.clase", en donde la
+# palabra "clase" debe ser sustituida por una clase existente...
+
+# Por ejemplo:
 x <- c("2", "34", "5", "8")
 class(x) # "character"
 (y <- as.numeric(x))
@@ -24,26 +30,32 @@ class(y) # "logical"
 (z <- as.numeric(y))
 # [1] 1 1 1 0 1 1 1 1
  
-## Así que "z" es distinto a "x", a pesar de que a priori uno puede
-## pensar que se hicieron caminos inversos, esto no es así ya que la
-## operación realizada (en total) no fue biyectiva.
- 
+# Así que "z" es distinto a "x", a pesar de que a priori uno puede
+# pensar que se hicieron caminos inversos, esto no es así ya que la
+# operación realizada (en total) no fue biyectiva.
+
+# También debe tenerse en cuenta que no todas las clases pueden
+# cohercionarse como queremos.
+
 # Otras funciones útiles son:
 as.matrix
 as.data.frame
 as.factor
 as.list
 as.vector
- 
+
+
 #### Identificadores
-## De manera similar, existen funciones que sirven para identificar
-## las clases de los objetos.
+# De manera similar, existen funciones que sirven para identificar las
+# clases de los objetos.
+
 x <- 1:5
-is.numeric(x) # Nótese que a pesar de que "x" es "integer", el resultado es TRUE
+is.numeric(x)
+# Nótese que a pesar de que "x" es "integer", el resultado es TRUE
  
 is.character(x) # FALSE
  
-#### Algunos identificadores comunes:
+# Algunos identificadores comunes:
 is.vector
 is.matrix
 is.double  # Si es un número con decimales o no.
@@ -54,27 +66,27 @@ is.array
 is.list
 is.logical
  
-## Debe destacarse que exsten algunos especiales:
+# Debe destacarse que exsten algunos especiales:
 is.na   # ¿Es NA?
 is.null # ¿Es NULL?
 is.nan  # ¿Es NaN?
  
-### Ejemplos:
-## NA viene a ser "Not Available" (no disponible)
+# Ejemplos:
+# NA viene a ser "Not Available" (no disponible)
 x <- c(4, 7, NA, 5)
-x == NA  ## No sirve usar este operador
+x == NA  # No sirve usar este operador
 # [1] NA NA NA NA
-is.na(x) ## sólo con is.na se puede identificar un "NA"
+is.na(x) # sólo con is.na se puede identificar un "NA"
 # [1] FALSE FALSE  TRUE FALSE
  
-## NaN es "Not a Number" (no es un número)
+# NaN es "Not a Number" (no es un número)
 log(-1) -> y # log no está definida para números negativos...
 # Warning message:
 # In log(-1) : NaNs produced
 y
 # [1] NaN
  
-## NULL viene a ser un "conjunto vacío"
+# NULL viene a ser un "conjunto vacío"
 x <- NULL
 x == NULL  # logical(0)
 is.null(x) # TRUE
