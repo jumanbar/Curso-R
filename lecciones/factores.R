4435281301bf2c32791a66ea4bd6c2eb18ceaee4
Juan Manuel <jumanbar@gmail.com>
2011-09-09 21:41:05 -0300

    Varios cambios a "factores.R":
     - "##" sustituido por "#"
     - Usar el término "variable categórica" para explicar qué son los factores.
     - Explicar el uso de "cut" para pasar de variable numérica a categórica.

--- lecciones/factores.R
+++ lecciones/factores.R
@@ -1,16 +1,18 @@
-### factores
+## Factores
  
-## Los objetos "factor" están pensados para trabajar dentro de fórmulas en R ("formula" es
-## otra clase de objetos en R). Una forma de comprender el concepto de factor es pensando
-## en su uso dentro del diseño experimental: un factor es una variable cuyo efecto se busca
-## entender; en un experimento se crean distintos tratamientos, cada uno con un nivel determinado
-## para dicho factor.
+# Los objetos "factor" son objetos que sirven para trabajar con variables
+# categóricas, como pueden ser franjas etareas, nombres de especies o
+# lugares. Una forma de comprender el concepto de factor es pensando en su
+# uso dentro del diseño experimental: un factor es una variable cuyo efecto
+# se busca entender; en un experimento se crean distintos tratamientos,
+# cada uno con un nivel determinado para dicho factor.
 
-## Ejemplo: efecto del nitrógeno en el crecimiento de un cultivo. Un tratamiento puede ser
-## sin N y otro con N. Entonces tenemos un factor ("N") con dos niveles (que pueden ser
-## denominados 0 y 1, o de cualquier otra forma).
+# Ejemplo: efecto del nitrógeno en el crecimiento de un cultivo. Un
+# tratamiento puede ser sin N y otro con N. Entonces tenemos un factor ("N")
+# con dos niveles (que pueden ser denominados 0 y 1, o de cualquier otra
+# forma).
  
-## Crear un factor con tres niveles, cinco casos de cada uno:
+# Crear un factor con tres niveles, cinco casos de cada uno:
 clasif <- rep(c('N1', 'N2', 'N3'), 5)
 x <- factor(clasif)
  
@@ -18,37 +20,56 @@ x <- factor(clasif)
 ?gl
 x <- gl(3, 5, labels=c('N1', 'N2', 'N3'))
  
-## Niveles (rótulos)
+# Niveles (rótulos)
 levels(x) # salida de clase "character"
 # [1] "N1" "N2" "N3"
 
-## Para obtener simplemente los rótulos de todas las observaciones, se
-## puede cohercionar el factor en un "character":
+# Para obtener simplemente los rótulos de todas las observaciones, se
+# puede cohercionar el factor en un "character":
 as.character(x)
 as.vector(x)
- 
-## ¿Cuántos niveles tiene el factor? fácil:
+
+# Nótese que los factores no son vectores:
+is.vector(x)
+
+# ¿Cuántos niveles tiene el factor? fácil:
 nlevels(x)
 # [1] 3
  
-## Para quitarle los rótulos:
+# Para quitarle los rótulos:
 y <- unclass(x)
-## Ahora lo puedo transformar en numeric:
+# Ahora lo puedo transformar en numeric:
 as.numeric(y)
-## Nótese que los números de este nuevo vector no tienen por qué
-## guardar relación alguna con los valores de los rótulos originales del
-## factor.
+# Nótese que los números de este nuevo vector no tienen por qué
+# guardar relación alguna con los valores de los rótulos originales del
+# factor.
 
-## Si los nombres de los rótulos son números, entonces se puede
-## convertir el factor en un numeric (con los valores de los rótulos),
-## usando:
+# Si los nombres de los rótulos son números, entonces se puede
+# convertir el factor en un numeric (con los valores de los rótulos),
+# usando:
 f <- gl(2, 3)
 as.numeric(levels(f))[f]
 
-## Nota final: no es nada raro confundir un objeto factor con uno
-## character, sobre todo cuando uno está aprendiendo R (y en particular
-## luego de importar matrices o data.frames). Por esto se recomienda
-## estar atento a la salida que R imprime en la consola (si es un factor
-## se imprimen los "Levels:..." al final. En el caso de importación de
-## datos, la opción por defecto es que las columnas con texto se interpetan
-## como factores. Esto se puede evitar usando "stringsAsFactor=FALSE"
+# Una variable numérica puede ser categorizada en un factor, usando la
+# función "cut". Por ejemplo, si asumimos que "edades" es un muestreo de
+# edades de estudiantes de la universidad:
+edades <- rpois(150, 25)
+
+# Podemos crear un factor de n categorías:
+(edades4 <- cut(edades, 4))
+
+# En la consola podemos ver en qué rango de edades cae cada observación y
+# cuáles son estos rangos ("Levels").
+
+# En definitiva, los factores son una construcción útil de R que está
+# pensada para trabajar con variables categóricas. En el curso se va a
+# profundizar en los usos de los factores, particularmente en los temas
+# relacionados con estadística y trabajo con datos.
+
+# Nota final: no es nada raro confundir un objeto factor con uno
+# character, sobre todo cuando uno está aprendiendo R (y en particular
+# luego de importar matrices o data.frames). Por esto se recomienda
+# estar atento a la salida que R imprime en la consola (si es un factor
+# se imprimen los "Levels:..." al final. En el caso de importación de
+# datos, la opción por defecto es que las columnas con texto se interpetan
+# como factores. Esto se puede evitar usando "stringsAsFactor=FALSE"
