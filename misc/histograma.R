444e290165ee78ecfffc02f20a9ee28750f6c83c
Juan Manuel Barreneche <jumanbar@gmail.com>
2012-07-29 09:46:57 -0300

    Ejemplo de script agregado

---
+++ misc/histograma.R
@@ -0,0 +1,7 @@
+# Grafica un histograma de una variable con distribución normal estándar. Agrega
+# una línea azul con la densidad y una roja con la densidad teórica.
+x <- rnorm(200)
+hist(x, freq=F)
+lines(density(x), col='blue', lwd=2)
+lines(density(x), col='blue', lwd=2)
+curve(dnorm(x), col='red', lwd=2, add=TRUE)
