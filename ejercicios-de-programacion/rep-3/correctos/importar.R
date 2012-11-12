a1b2190c67d6950c8ef11d97e36371d130605efa
Juan Manuel Barreneche <jumanbar@gmail.com>
2012-11-12 01:28:53 -0300

    Cambio de nombre de carpeta rep-4 a rep-5 y agregado el archivo fantasma del cuestionario 5.4

--- ejercicios-de-programacion/rep-3/correctos/importar.R
+++ ejercicios-de-programacion/rep-3/correctos/importar.R
@@ -10,7 +10,7 @@
 
 usa <- read.table('usa.csv', header = TRUE, row.names = 1)
 names(usa) <- c('Abrev', 'Poblacion', 'Ingresos', 'Analf', 'Esp.Vida',
-                            'Homicidio', 'Sec.Grad', 'Heladas', 'Area', 'Division')
+                'Homicidio', 'Sec.Grad', 'Heladas', 'Area', 'Division')
 
 levels(usa$Division) <- c('Noreste Central', 'Sudeste Central', 
                           'Atlantico Central', 'Montania', 'Nueva Inglaterra', 
