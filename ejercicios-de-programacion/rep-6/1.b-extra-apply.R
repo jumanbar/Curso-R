3617f0b12eecc2fd7751ee547b16a2852e694c81
Juan Manuel Barreneche <jmbarreneche@uc.cl>
2013-06-18 14:52:36 -0400

    Cambios 1.b: cuenta mayores a 45

--- ejercicios-de-programacion/rep-6/1.b-extra-apply.R
+++ ejercicios-de-programacion/rep-6/1.b-extra-apply.R
@@ -6,10 +6,12 @@
 
 # Objetivos:
 # Usar la función apply, no pueden haber alternativas, como while o for.
-# f: función que suma la cantidad de NAs que tiene un vector cualquiera. No es
-#    obligatorio crear f: la función puede ser definida en la misma línea en
-#    que se ejecuta apply (buscar por "anonymous function" en la web)
-# out*: vector con las cantidades de NAs de las filas de la matriz datos.
+# f: función que suma la cantidad de valores mayores a 45 que tiene un vector 
+#    cualquiera. No es obligatorio crear f: la función puede ser definida en 
+#    la misma línea en que se ejecuta apply (buscar por "anonymous function" 
+#    en la web)
+# out*: vector con las cantidades de valores mayores a 45 en las filas de la 
+# matriz datos.
 
 
 #===== Su código comienza aquí: =====#
