ba067d0b64df192b9cc020aeec563180f8f1ae61
Juan Manuel Barreneche <jmbarreneche@uc.cl>
2013-06-18 15:50:36 -0300

    Cambios: ya no cuenta NAs

--- ejercicios-de-programacion/rep-6/1.a-loop-for.R
+++ ejercicios-de-programacion/rep-6/1.a-loop-for.R
@@ -1,17 +1,17 @@
 # Rep. VI - ej. 1.a
 
 # Instrucciones: 
-# Debe utilizar un loop for para determinar la cantidad de NAs en cada fila de
-# la matriz datos. Las sumas se almacenarán en el vector out, el cual debe
-# tener la longitud adecuada antes de iniciarse el loop.
+# Debe utilizar un loop for para determinar la cantidad de valores mayores a 45
+# en cada fila de la matriz datos. Las sumas se almacenarán en el vector out, 
+# el cual debe tener la longitud adecuada antes de iniciarse el loop.
 
 # Objetivos:
 # Utilizar el loop for. No se permiten while, apply ni similares.
-# out*: vector con las cantidades de NAs de las filas de la matriz datos.
+# out*: vector con las cantidades de valores mayores a 45 en 
+#       las filas de la matriz datos.
 
 # Generación de la matriz datos:
 datos <- matrix(rpois(rpois(1, 125) * 15, 43), ncol = 15)
-datos[sample(length(datos), rpois(1, 250))] <- NA
 
 #===== Su código comienza aquí: =====#
 
